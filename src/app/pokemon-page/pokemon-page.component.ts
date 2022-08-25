4398b0bede8d1f4e90958b1ecdba2e01c2766c61
@antonKapustin <anton.kapustin@neatsoft.io>
2022-08-25 16:10:55 +0300

    update hashcange

--- src/app/pokemon-page/pokemon-page.component.ts
+++ src/app/pokemon-page/pokemon-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { BehaviorSubject } from "rxjs";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { BehaviorSubject, map, Observable, Subscription } from "rxjs";
 import { HttpService, Pokemon } from "../services/http/http.service";
 import { Location } from "@angular/common";
 import { PokedexService } from "../services/pokedex/pokedex.service";
@@ -10,10 +10,11 @@ import { PokedexService } from "../services/pokedex/pokedex.service";
   templateUrl: "./pokemon-page.component.html",
   styleUrls: ["./pokemon-page.component.scss"],
 })
-export class PokemonPageComponent implements OnInit {
+export class PokemonPageComponent implements OnInit, OnDestroy {
   pokemon!: Pokemon;
   loading$: BehaviorSubject<boolean> = this.httpService.getLoadingState();
   added = true;
+  subscriptions: Subscription[] = [];
   constructor(
     private httpService: HttpService,
     private router: ActivatedRoute,
@@ -22,11 +23,19 @@ export class PokemonPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    let name = this.router.snapshot.params["name"];
-    this.added = this.pokedex.isAdded(name);
-    this.httpService
-      .loadPokemon(name)
+    let subscriptions1$ = this.router.paramMap
+      .pipe(map((param: ParamMap) => param.get("name") as string))
+      .subscribe((value) => {
+        this.filterPokemons(value);
+      });
+    this.subscriptions.push(subscriptions1$);
+  }
+  filterPokemons(value: string) {
+    this.added = this.pokedex.isAdded(value);
+    let subscriptions1$ = this.httpService
+      .loadPokemon(value)
       .subscribe((value) => (this.pokemon = value));
+    this.subscriptions.push(subscriptions1$);
   }
   onNavigateBack() {
     this.location.back();
@@ -38,5 +47,10 @@ export class PokemonPageComponent implements OnInit {
   removeFromPokedex() {
     this.pokedex.removeFromPokedex(this.pokemon);
     this.added = false;
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((item) => {
+      item.unsubscribe();
+    });
   }
 }
